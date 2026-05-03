5239e4e5da79c9c57a4315ed48cb10d6373b393a
Yuval Varke <varke.vishu01@gmail.com>
2026-05-03 17:40:04 +0530

    Added Gemini client for the LLM response

--- src/helper.py
+++ src/helper.py
@@ -2,8 +2,15 @@ import fitz
 import os 
 from dotenv import load_dotenv
 load_dotenv()
+from google import genai
+from google.genai import types
 
 
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
+
+
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 
 def extract_text_from_pdf(uploaded_file):
@@ -22,3 +29,29 @@ def extract_text_from_pdf(uploaded_file):
     for page in doc:
         text += page.get_text()
     return text
+
+
+
+def ask_gemini(prompt, max_tokens=500):
+    """
+    Sends a prompt to the Gemini API and returns the response.
+
+    Args:
+        prompt (str): The prompt to send to the Gemini API.
+        model (str): The model to use for the request.
+        temperature (float): The temperature for the response.
+        
+    Returns:
+        str: The response from the Gemini API.
+    """
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            temperature=0.1,
+            max_output_tokens=max_tokens
+        )
+    )
+    
+    return response.text
